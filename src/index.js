38c0b65a7048c6944b5179ee3933d5f31f85dff6
ElenaZhukouskaya <elenazhukouskaya@gmail.com>
2022-01-29 23:37:40 +0300

    feat: add function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const revInt = Math.abs(n).toString().split("").reverse().join("");
+    return +revInt;
+};
